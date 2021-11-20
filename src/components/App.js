42c862d20c3f54328fea5c2a0460b1027edf3bba
tinlee <tinashediego@gmail.com>
2021-11-20 20:34:27 +0200

    connecting to frontend

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+
 import { Tabs, Tab } from 'react-bootstrap'
 import dBank from '../abis/dBank.json'
 import React, { Component } from 'react';
@@ -6,8 +7,6 @@ import dbank from '../dbank.png';
 import Web3 from 'web3';
 import './App.css';
 
-//h0m3w0rk - add new tab to check accrued interest
-
 class App extends Component {
 
   async componentWillMount() {
@@ -15,27 +14,78 @@ class App extends Component {
   }
 
   async loadBlockchainData(dispatch) {
+    if(typeof window.ethereum!=='undefined'){
+      const web3 = new Web3(window.ethereum)
+      const netId = await web3.eth.net.getId()
+      const accounts = await web3.eth.getAccounts()
 
-    //check if MetaMask exists
+      //load balance
+      if(typeof accounts[0] !=='undefined'){
+        const balance = await web3.eth.getBalance(accounts[0])
+        this.setState({account: accounts[0], balance: balance, web3: web3})
+      } else {
+        window.alert('Please login with MetaMask')
+      }
 
-      //assign to values to variables: web3, netId, accounts
+      //load contracts
+      try {
+        const token = new web3.eth.Contract(Token.abi, Token.networks[netId].address)
+        const dbank = new web3.eth.Contract(dBank.abi, dBank.networks[netId].address)
+        const dBankAddress = dBank.networks[netId].address
+        this.setState({token: token, dbank: dbank, dBankAddress: dBankAddress})
+      } catch (e) {
+        console.log('Error', e)
+        window.alert('Contracts not deployed to the current network')
+      }
 
-      //check if account is detected, then load balance&setStates, elsepush alert
-
-      //in try block load contracts
-
-    //if MetaMask not exists push alert
+    } else {
+      window.alert('Please install MetaMask')
+    }
   }
 
   async deposit(amount) {
-    //check if this.state.dbank is ok
-      //in try block call dBank deposit();
+    if(this.state.dbank!=='undefined'){
+      try{
+        await this.state.dbank.methods.deposit().send({value: amount.toString(), from: this.state.account})
+      } catch (e) {
+        console.log('Error, deposit: ', e)
+      }
+    }
   }
 
   async withdraw(e) {
-    //prevent button from default click
-    //check if this.state.dbank is ok
-    //in try block call dBank withdraw();
+    e.preventDefault()
+    if(this.state.dbank!=='undefined'){
+      try{
+        await this.state.dbank.methods.withdraw().send({from: this.state.account})
+      } catch(e) {
+        console.log('Error, withdraw: ', e)
+      }
+    }
+  }
+
+  async borrow(amount) {
+    if(this.state.dbank!=='undefined'){
+      try{
+        await this.state.dbank.methods.borrow().send({value: amount.toString(), from: this.state.account})
+      } catch (e) {
+        console.log('Error, borrow: ', e)
+      }
+    }
+  }
+
+  async payOff(e) {
+    e.preventDefault()
+    if(this.state.dbank!=='undefined'){
+      try{
+        const collateralEther = await this.state.dbank.methods.collateralEther(this.state.account).call({from: this.state.account})
+        const tokenBorrowed = collateralEther/2
+        await this.state.token.methods.approve(this.state.dBankAddress, tokenBorrowed.toString()).send({from: this.state.account})
+        await this.state.dbank.methods.payOff().send({from: this.state.account})
+      } catch(e) {
+        console.log('Error, pay off: ', e)
+      }
+    }
   }
 
   constructor(props) {
@@ -56,7 +106,7 @@ class App extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href="#"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -66,15 +116,97 @@ class App extends Component {
         </nav>
         <div className="container-fluid mt-5 text-center">
         <br></br>
-          <h1>{/*add welcome msg*/}</h1>
-          <h2>{/*add user address*/}</h2>
+          <h1>Welcome to dBank</h1>
+          <h2>{this.state.account}</h2>
           <br></br>
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
               <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-                {/*add Tab deposit*/}
-                {/*add Tab withdraw*/}
+                <Tab eventKey="deposit" title="Deposit">
+                  <div>
+                  <br></br>
+                    How much do you want to deposit?
+                    <br></br>
+                    (min. amount is 0.01 ETH)
+                    <br></br>
+                    (1 deposit is possible at the time)
+                    <br></br>
+                    <form onSubmit={(e) => {
+                      e.preventDefault()
+                      let amount = this.depositAmount.value
+                      amount = amount * 10**18 //convert to wei
+                      this.deposit(amount)
+                    }}>
+                      <div className='form-group mr-sm-2'>
+                      <br></br>
+                        <input
+                          id='depositAmount'
+                          step="0.01"
+                          type='number'
+                          ref={(input) => { this.depositAmount = input }}
+                          className="form-control form-control-md"
+                          placeholder='amount...'
+                          required />
+                      </div>
+                      <button type='submit' className='btn btn-primary'>DEPOSIT</button>
+                    </form>
+
+                  </div>
+                </Tab>
+                <Tab eventKey="withdraw" title="Withdraw">
+                  <br></br>
+                    Do you want to withdraw + take interest?
+                    <br></br>
+                    <br></br>
+                  <div>
+                    <button type='submit' className='btn btn-primary' onClick={(e) => this.withdraw(e)}>WITHDRAW</button>
+                  </div>
+                </Tab>
+                <Tab eventKey="borrow" title="Borrow">
+                  <div>
+
+                  <br></br>
+                    Do you want to borrow tokens?
+                    <br></br>
+                    (You'll get 50% of collateral, in Tokens)
+                    <br></br>
+                    Type collateral amount (in ETH)
+                    <br></br>
+                    <br></br>
+                    <form onSubmit={(e) => {
+
+                      e.preventDefault()
+                      let amount = this.borrowAmount.value
+                      amount = amount * 10 **18 //convert to wei
+                      this.borrow(amount)
+                    }}>
+                      <div className='form-group mr-sm-2'>
+                        <input
+                          id='borrowAmount'
+                          step="0.01"
+                          type='number'
+                          ref={(input) => { this.borrowAmount = input }}
+                          className="form-control form-control-md"
+                          placeholder='amount...'
+                          required />
+                      </div>
+                      <button type='submit' className='btn btn-primary'>BORROW</button>
+                    </form>
+                  </div>
+                </Tab>
+                <Tab eventKey="payOff" title="Payoff">
+                  <div>
+
+                  <br></br>
+                    Do you want to payoff the loan?
+                    <br></br>
+                    (You'll receive your collateral - fee)
+                    <br></br>
+                    <br></br>
+                    <button type='submit' className='btn btn-primary' onClick={(e) => this.payOff(e)}>PAYOFF</button>
+                  </div>
+                </Tab>
               </Tabs>
               </div>
             </main>
